bda6ce2ecf403b5698ac9a06a4ded3a0433cfaab
chaqui <josuechaqui@ubuchaqui.(none)>
2012-07-03 11:42:24 -0600

    readme sobre el proyecto

--- Vehiculos_Y_Maquinaria.py
+++ Vehiculos_Y_Maquinaria.py
@@ -1,11 +1,10 @@
-'''
-Created on 3/07/2012
-
-@author: josuechaqui
-'''
 from Garantia_Prendaria import GarantiaPrendaria
+from Tarjeta_De_Circulacion import TarjetaDeCirculacion
+
 class VehiculosYMaquinaria(GarantiaPrendaria):
     def __init__(self):
         GarantiaPrendaria.__init__(self)
+        self.tarjetaDeCirculacion=TarjetaDeCirculacion()
+        
         
         
