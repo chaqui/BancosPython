457a6ac0c834c4b33faa2d21133fa1e0abf72589
chaqui <josuechaqui@ubuchaqui.(none)>
2012-07-03 12:05:32 -0600

    modificacion y nuevos modulos

---
+++ Clase_Convencional.py
@@ -0,0 +1,12 @@
+'''
+Created on 3/07/2012
+
+@author: josuechaqui
+'''
+
+class Convencional(object):
+
+
+    def __init__(self):
+        pass
+        
--- Vehiculos_Y_Maquinaria.py
+++ Vehiculos_Y_Maquinaria.py
@@ -5,6 +5,18 @@ class VehiculosYMaquinaria(GarantiaPrendaria):
     def __init__(self):
         GarantiaPrendaria.__init__(self)
         self.tarjetaDeCirculacion=TarjetaDeCirculacion()
-        
-        
+    def imprimir(self):
+        GarantiaPrendaria.imprimir(self)
+        self.tarjetaDeCirculacion.imprimir()
+        print"Cuota es de: Q.%f0.2"%(self.cuota)
+    def calcular(self):
+        self.cuota=self.monto*(0.015/(1-(1+0.015)**((self.cuotas)*-1)))
+    def Guardar(self):
+        try:
+            archivo=open("Archivos/VehiculosYMaquinaria.txt","a")
+        except:
+            archivo=open("Archivos/VehiculosYMaquinaria.txt","w")
+        archivo.write(self.cliente.nombre+" "+self.cliente.segudnonombre+" "+self.cliente.primerapellido+" "+self.cliente.segundoapellido+" "+self.cliente.dirreccioncliente+" "+self.cliente.correocliente+" "+str(self.cliente.telfonocliente)+" "+str(self.tarjetaDeCirculacion.numero)+" "+str(self.tarjetaDeCirculacion.fechaDeEmision)+" "+str(self.tarjetaDeCirculacion.fechaDeCaducidad)+" "+str(self.monto)+" "+str(self.cuotas)+" "+str(self.cuota)+" "+self.nocredito+"¬")
+        archivo.close()
+    
         
