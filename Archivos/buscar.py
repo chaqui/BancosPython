f75073c170913ff9e745bd7600a1842ea1f3b7f2
chaqui <josuechaqui@ubuchaqui.(none)>
2012-07-06 13:43:31 -0600

    agregando buscar

---
+++ Archivos/buscar.py
@@ -0,0 +1,13 @@
+class Buscar(object):
+    def __init__(self):
+        self.busqueda=raw_input("ingrese el nombre a buscar: ")
+    def Busqueda(self):
+        self.archivo=('Archivos/Clase_Convencional.txt','Clase_No_Convencional.txt','Archivos/Fiduciario_Privado.txt','Archivos/FiduciarioPublico.txt','Archivos/HipotecaPosesoria.txt','Archivos/HipotecaReal.txt')
+        
+        
+        
+                    
+buscar=Buscar() 
+buscar.Busqueda()           
+
+            
